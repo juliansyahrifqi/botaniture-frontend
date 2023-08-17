3d0cc5e08f526082ceb6a4c89b2ba56da0594398
juliansyahrifqi <rifqipratama9@gmail.com>
2023-08-17 11:41:53 +0700

    add hover background to sibebar button

--- src/app/components/Button/ButtonSidebar.tsx
+++ src/app/components/Button/ButtonSidebar.tsx
@@ -3,7 +3,7 @@ import { BiMenuAltLeft } from "react-icons/bi";
 export default function ButtonSidebar({ handleSidebarOpen }: any) {
   return (
     <button
-      className="border-none outline-none p-3 bg-primary bg-opacity-30 cursor-pointer rounded-xl"
+      className="border-none outline-none p-3 bg-primary bg-opacity-30 cursor-pointer rounded-xl hover:bg-opacity-50 transition-colors duration-300 ease-in-out"
       onClick={handleSidebarOpen}
     >
       <BiMenuAltLeft className="text-3xl text-primary font-bold" />
--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -2,25 +2,6 @@ import { ProductType } from "@/app/types/productType";
 import CardDiscount from "./CardDiscount";
 import CardOffer from "./CardOffer";
 
-const data = [
-  {
-    product_id: 1,
-    product_name: 'Zamiculcas Zamiifolia ZZ Plant / Tanaman Dolar',
-    product_price: 400000,
-    product_discount: 10,
-    product_image: '/tanaman-1.png',
-    product_slug: 'zamiculcas'
-  },
-  {
-    product_id: 2,
-    product_name: 'Monstera  Deliciosa / Tamaman Janda Bolong',
-    product_price: 250000,
-    product_discount: 12,
-    product_image: '/tanaman-2.png',
-    product_slug: 'montsera'
-  }
-]
-
 export default function Hero(props: any) {
   const { promo } = props;
 
